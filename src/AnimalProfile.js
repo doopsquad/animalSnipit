512ad8e0b7acf49f46930515e2f66e614c0b8cee
doopsquad <estebancalvocruz@gmail.com>
2024-03-23 17:44:06 -0400

    joe

--- src/AnimalProfile.js
+++ src/AnimalProfile.js
@@ -11,27 +11,8 @@ const results = () => {
 
 return (
     <div className="profile">
+      <img id="img" src={props.img} onClick={results} />
       <h1 id="name">{props.name}</h1>
-      <div className='media'>
-      <img id="img" src={props.img} width="320" height="240" />
-        <video
-        id="video"
-        controls
-        source
-        src={props.video}
-        type="video/mp4"
-        width="320"
-        height="240"
-        autoPlay
-        loop
-      />
-
-      </div>
-      <article id="info">
-        {props.info}
-      </article>
-
-      <button id="vote" onClick={results}>VOTE!</button>
     </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -4,62 +4,65 @@ import "./Style.css";
 
 function App() {
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [topIndex, setTopIndex] = useState(0);
+  const [bottomIndex, setBottomIndex] = useState(1);
+  const [animalCount, setAnimalCount] = useState(0);
 
   const animalData = [
     {
       name: "basking shark",
       img: "https://th.bing.com/th/id/R.db6acb92804f7d7e0a645ca7d3dddbc2?rik=zOVzXpWAq6HNHg&riu=http%3a%2f%2fanimalsadda.com%2fwp-content%2fuploads%2f2013%2f11%2fBasking-Shark-3.jpg&ehk=UJTo17LUZLuEINcA23wi7Qrfn%2bEv%2bDC9NpghytAD33U%3d&risl=&pid=ImgRaw&r=0",
-      video: "https://doopsquad.github.io/React-app/whiteRhino.mp4",
-      info: "The White Rhino is one of the largest and most recognizable species of rhinoceros...",
     },
     {
       name: "whale shark",
       img: "https://www.thoughtco.com/thmb/o3_yk19FVHEzfbNlIroah5ZqZjs=/4632x3088/filters:no_upscale():max_bytes(150000):strip_icc()/whale-shark-514475851-e56169bf49fb41a49f3b30068f806789.jpg",
-      video: "https://doopsquad.github.io/React-app/asianElephant.mp4",
-      info: "The Asian Elephant is a majestic species found throughout Southeast Asia's forests and grasslands...",
     },
     {
-      name:"rhino",
-      img:"https://th.bing.com/th/id/OIP.Kw6EzySU7iAc6sue1y0DKgHaEK?rs=1&pid=ImgDetMain", 
-      video:"https://doopsquad.github.io/React-app/whiteRhino.mp4", 
-      info: "placeholder",
+      name: "rhino",
+      img: "https://th.bing.com/th/id/OIP.Kw6EzySU7iAc6sue1y0DKgHaEK?rs=1&pid=ImgDetMain",
     },
     {
-      name:"asian elephant", 
-      img:"https://www.thoughtco.com/thmb/q4t3OQkJIwiyTHnV4Pve34f4Ygo%3D/2250x1500/filters:fill(auto%2C1)/167003501-56a0089e5f9b58eba4ae8f93.jpg", 
-      video:"https://doopsquad.github.io/React-app/asianElephant.mp4", 
-      info:"placeholder"
-    }
-    
+      name: "asian elephant",
+      img: "https://www.thoughtco.com/thmb/q4t3OQkJIwiyTHnV4Pve34f4Ygo%3D/2250x1500/filters:fill(auto%2C1)/167003501-56a0089e5f9b58eba4ae8f93.jpg",
+    },
   ];
 
-  const handleVote = (vote) => {
+  const changeTop = () => {
+    setTopIndex((prevTopIndex) => {
+      let newTopIndex = (prevTopIndex + 1) % animalData.length;
+      while (newTopIndex === bottomIndex) {
+        newTopIndex = (newTopIndex + 1) % animalData.length;
+      }
+      return newTopIndex;
+    });
+    setAnimalCount((prevCount) => prevCount + 1);
+  };
+  
+  const changeBottom = () => {
+    setBottomIndex((prevBottomIndex) => {
+      let newBottomIndex = (prevBottomIndex + 1) % animalData.length;
+      while (newBottomIndex === topIndex) {
+        newBottomIndex = (newBottomIndex + 1) % animalData.length;
+      }
+      return newBottomIndex;
+    });
+    setAnimalCount((prevCount) => prevCount + 1);
+  };
 
-    const nextIndex = vote === "left" ? currentIndex - 1 : currentIndex + 1;
-  
-    const newIndex = nextIndex < 0 ? animalData.length - 1 : nextIndex % animalData.length;
-  
-    setCurrentIndex(newIndex);
-  }
-  
+
   return (
     <div className="App">
-     <AnimalProfile
-      name={animalData[currentIndex].name}
-      img={animalData[currentIndex].img}
-      video={animalData[currentIndex].video}
-      info={animalData[currentIndex].info}
-    />
-    <h1 id="versus">VERSUS</h1>
-    <AnimalProfile
-      name={animalData[(currentIndex + 1) % animalData.length].name}
-      img={animalData[(currentIndex + 1) % animalData.length].img}
-      video={animalData[(currentIndex + 1) % animalData.length].video}
-      info={animalData[(currentIndex + 1) % animalData.length].info}
-    />
-    <button onClick={() => handleVote("left")}>Vote Left</button>
-    <button onClick={() => handleVote("right")}>Vote Right</button>
+      <AnimalProfile img={animalData[topIndex].img} name={animalData[topIndex].name} />
+      <AnimalProfile img={animalData[bottomIndex].img} name={animalData[bottomIndex].name} />
+      <h1 id="animalCounter">{animalCount}</h1>
+      <div id="buttonHolder">
+        <button id="btnLeft" className="button" onClick={changeTop}>
+          Change Top
+        </button>
+        <button id="btnRight" className="button" onClick={changeBottom}>
+          Change Bottom
+        </button>
+      </div>
     </div>
   );
 }
