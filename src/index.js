9345f63da955b39528734e79d31c829d59a408dd
doopsquad <estebancalvocruz@gmail.com>
2024-03-23 21:17:16 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANjqQtZm7hPqmHl1ilgApWDRkRRqf5S-0",
+  authDomain: "animal-snipet.firebaseapp.com",
+  projectId: "animal-snipet",
+  storageBucket: "animal-snipet.appspot.com",
+  messagingSenderId: "137804291186",
+  appId: "1:137804291186:web:f98ace37b6415e271c36eb",
+  measurementId: "G-69WQ6RGC1L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
